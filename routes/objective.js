6dc49688e6fd9cd5fbecef7a0c05135532b42819
wonseok <mynaman85@gmail.com>
2018-08-24 20:41:32 +0900

    router working

--- routes/objective.js
+++ routes/objective.js
@@ -3,20 +3,37 @@ const { Objective } = require('../models');
 
 const router = express.Router();
 
-// router.get('/', (req, res) =>{
-//     console.log('파라미터가 없는 라우터');
-//     // res.render('objective', { name : 'jsman', age : 35, add : '인천'});
-// });
+router.get('/', (req, res) =>{
+    console.log('파라미터가 없는 라우터');
+    res.render('objectiveEdit', {});
+});
+
+router.get('/:id/modify', (req, res) =>{        
+    const id = parseInt(req.params.id, 10);
+    if(Number.isNaN(id)) return res.send('Not Date');
+    Objective.findOne({ where : {id}})
+                    .then((row) => {
+                        if(!row) res.send('Not Date');
+                        console.log(row.get());                        
+                        return res.render('objectiveEdit', {row : row.get()});
+                    })
+                    .catch((err) =>{
+                        console.error(err);
+                        return err;
+                    });
+});
 
 
-router.get('/:id', (req, res) =>{
-    const id = req.params.id;
-    if(Number.isNaN(parseInt(id),10)) res.render('test', {});    
-    Objective.find({ where: {id} })
-                    .then((result) =>{
-                        if(!result) res.send('NOT DATE')
-                        console.log(result);
-                        return res.send(JSON.stringify(result));
+
+
+router.get('/:id', (req, res) =>{    
+    const id = parseInt(req.params.id, 10);
+    if(Number.isNaN(id)) return res.send('Not Date');
+    Objective.findOne({ where: {id} })
+                    .then((row) =>{
+                        if(!row) res.send('Not Date');
+                        console.log(row.get());
+                        return res.render('objectiveView', {row : row.get() });
                     })
                     .catch((err) => {
                         console.error(err);
@@ -24,19 +41,9 @@ router.get('/:id', (req, res) =>{
                     });
 });
 
-router.post('/', (req, res) => {
-
-    console.log(req.body);
-    console.log(JSON.stringify(req.body));
-
-    var obj = req.body;
-    var result = Object.keys(obj).map(function(key) {
-        return [ key , obj[key]];
-      });
-    console.log('결과');
-    console.log(result);
-
-    Objective.bulkCreate(data).then((result) => {
+router.post('/', (req, res) => {       
+    const obj = req.body;    
+    Objective.create(obj).then((result) => {
         if(!result){
             throw error;
         }
@@ -47,33 +54,6 @@ router.post('/', (req, res) => {
         return err;
     });    
 });
-
-// router.post('/', (req, res) => {
-       
-//     var count =  Object.keys(req.body).length;
-//     console.log(count);
-//     Objective.create({
-//         obj1_1 : req.body.obj1_1,
-//         obj1_2 : req.body.obj1_2,
-//         obj1_3 : req.body.obj1_3,
-//         obj1_4 : req.body.obj1_4,
-//         obj1_5 : req.body.obj1_5,
-//         obj1_6 : req.body.obj1_6,
-//         obj1_7 : req.body.obj1_7,
-//         obj1_8 : req.body.obj1_8,        
-//         obj1_subject : req.body.obj1_subject,
-//     }).then((result) => {
-//         if(!result){
-//             throw error;
-//         }
-//         const id = result.get().id;        
-//         res.redirect(`/objective/${id}`);
-//     }).catch((err) =>{
-//         console.error(err);
-//         return err;
-//     });    
-// });
-
 
 module.exports = router;
 
--- routes/page.js
+++ routes/page.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-     res.render('test', { title : 'mandalart'});
+     res.render('objectiveEdit', { title : 'mandalart'});
 });
 
 module.exports = router;
