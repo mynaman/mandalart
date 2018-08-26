329d7e0dc82bbdc7c306ec8430ebc57005677a2f
WonSeck <mynaman85@gmail.com>
2018-08-26 23:25:01 +0900

    code modify

--- routes/objective.js
+++ routes/objective.js
@@ -1,56 +1,69 @@
 const express = require('express');
 const { Objective } = require('../models');
+const obj = require('./test');
 
 const router = express.Router();
+
 
 router.get('/', (req, res) =>{
     console.log('파라미터가 없는 라우터');
     res.render('objectiveEdit', {});
 });
 
-router.get('/:id/modify', (req, res) =>{        
-    const id = parseInt(req.params.id, 10);
-    if(Number.isNaN(id)) return res.send('Not Date');
-    Objective.findOne({ where : {id}})
-                    .then((row) => {
-                        if(!row) res.send('Not Date');
-                        console.log(row.get());                        
-                        return res.render('objectiveEdit', {row : row.get()});
-                    })
-                    .catch((err) =>{
-                        console.error(err);
-                        return err;
-                    });
-});
+// router.get('/:id/modify', (req, res) =>{        
+//     const id = parseInt(req.params.id, 10);
+//     if(Number.isNaN(id)) return res.send('Not Date');
+//     Objective.findOne({ where : {id}})
+//                     .then((row) => {
+//                         if(!row) res.send('Not Date');
+//                         console.log(row.get());                        
+//                         return res.render('objectiveEdit', {row : row.get()});
+//                     })
+//                     .catch((err) =>{
+//                         console.error(err);
+//                         return err;
+//                     });
+// });
 
-router.get('/:id', (req, res) =>{    
-    const id = parseInt(req.params.id, 10);
-    if(Number.isNaN(id)) return res.send('Not Date');
-    Objective.findOne({ where: {id} })
-                    .then((row) =>{
-                        if(!row) res.send('Not Date');
-                        console.log(row.get());
-                        return res.render('objectiveView', {row : row.get() });
-                    })
-                    .catch((err) => {
-                        console.error(err);
-                        return err;
-                    });
-});
+// router.get('/:id', (req, res) =>{    
+//     const id = parseInt(req.params.id, 10);
+//     if(Number.isNaN(id)) return res.send('Not Date');
+//     Objective.findOne({ where: {id} })
+//                     .then((row) =>{
+//                         if(!row) res.send('Not Date');
+//                         console.log(row.get());
+//                         return res.render('objectiveView', {row : row.get() });
+//                     })
+//                     .catch((err) => {
+//                         console.error(err);
+//                         return err;
+//                     });
+// });
 
-router.post('/', (req, res) => {       
-    const obj = req.body;    
-    Objective.create(obj).then((result) => {
-        if(!result){
-            throw error;
-        }
-        const id = result.get().id;        
-        res.redirect(`/objective/${id}`);
-    }).catch((err) =>{
+// router.post('/', (req, res) => {       
+//     const obj = req.body;    
+//     Objective.create(obj).then((result) => {
+//         if(!result){
+//             throw error;
+//         }
+//         const id = result.get().id;        
+//         res.redirect(`/objective/${id}`);
+//     }).catch((err) =>{
+//         console.error(err);
+//         return err;
+//     });    
+// });
+
+router.post('/', (req, res) => {
+    Objective.create(obj).then((result) =>{
+        if(!result) throw error
+        res.send(JSON.stringify(result));
+    }).catch((err) => {
         console.error(err);
         return err;
-    });    
+    });
 });
+
 
 module.exports = router;
 
--- routes/page.js
+++ routes/page.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-     res.render('objectiveEdit', { title : 'mandalart'});
+     res.render('objectiveView', {});
 });
 
 module.exports = router;
